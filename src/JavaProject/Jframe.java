cc034daad594fdf9e90f9e8476b6063a7e96d17e
Faisal Khan <faisal3325@users.noreply.github.com>
2016-03-08 21:16:11 +0530

    Added files via upload
    Constraint on ADD button

--- src/JavaProject/Jframe.java
+++ src/JavaProject/Jframe.java
@@ -2,25 +2,30 @@ package JavaProject;
 
 import java.awt.EventQueue;
 
-import java.awt.Window;
 import javax.swing.JFrame;
+
 import java.awt.CardLayout;
+
+import javax.swing.DefaultListModel;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+
 import java.awt.Font;
+
 import javax.swing.JTextArea;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+
 import javax.swing.JFormattedTextField;
-import javax.swing.JPopupMenu;
-import java.awt.Component;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+
 import javax.swing.JList;
-import java.awt.Panel;
+
+import java.util.ArrayList;
 import java.awt.List;
+
 
 
 public class Jframe {
@@ -31,6 +36,7 @@ public class Jframe {
 	public JPanel frame2;
 	static String groups;
 	static String area;
+	int a = 0;
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -87,13 +93,10 @@ public class Jframe {
 				else
 				{
 					frame1.setVisible(false);
-					//frame1.dispose();
 					
 					final JPanel frame2 = new JPanel();
 					frame.getContentPane().add(frame2, "name_9113232328555");
 					frame2.setLayout(null);
-					
-					 
 					
 					JLabel lblLocation = new JLabel("Location:-");
 					lblLocation.setFont(new Font("Tahoma", Font.PLAIN, 20));
@@ -105,32 +108,31 @@ public class Jframe {
 					frame2.add(textField_1);
 					textField_1.setColumns(10);
 					
-					JTextArea textArea = new JTextArea();
-					textArea.setBounds(59, 104, 220, 146);
-					frame2.add(textArea);
-					
-					JButton btnA = new JButton("ADD");
-					btnA.addActionListener(new ActionListener() {
-						public void actionPerformed(ActionEvent e) {
-							System.out.println("Kaam Krra saala");
-							JList list_1 = new JList();
-							list_1.setBounds(57, 106, 187, 110);
-							frame2.add(list_1);
-							String[] data = {};
-							int i, loop = Integer.parseInt(groups);
-							for(i = 0; i < loop; i++)
-							{
-								area = textField_1.getText();
-								list_1.add(new JList(data));	
-								list_1.setVisible(true);
+					int i, loop = Integer.parseInt(groups);
+					System.out.println(loop);
+					List list = new List();
+					list.setBounds(39, 95, 240, 124);
+					frame2.add(list);
+					for(i = 0; i < loop; i++)	{
+						JButton btnA = new JButton("ADD");
+						btnA.addActionListener(new ActionListener() {
+							public void actionPerformed(ActionEvent e) {
+								//System.out.println("Kaam Krra saala");
+								a++;
+								if(a <= loop)
+								{
+									ArrayList<String> str = new ArrayList<String>();
+									area = textField_1.getText();
+									str.add(area);
+									list.add(area);
+									System.out.println(area);
+							
+								}
 							}
-							System.out.println(area);
-							//new city = textfield_1.getText();
-						}
-					});
-					btnA.setBounds(355, 46, 69, 23);
-					frame2.add(btnA);
-					
+						});
+						btnA.setBounds(355, 46, 69, 23);
+						frame2.add(btnA);
+					}
 					JButton btnNewButton_1 = new JButton("Locate On Map");
 					btnNewButton_1.addActionListener(new ActionListener() {
 						public void actionPerformed(ActionEvent arg0) {
@@ -139,8 +141,6 @@ public class Jframe {
 					});
 					btnNewButton_1.setBounds(301, 150, 123, 36);
 					frame2.add(btnNewButton_1);
-					
-									
 					frame2.setVisible(true);
 				}	
 			}
@@ -154,8 +154,6 @@ public class Jframe {
 		frame.getContentPane().add(frame2, "name_9113232328555");
 		frame2.setLayout(null);
 		
-		 
-		
 		JLabel lblLocation = new JLabel("Location:-");
 		lblLocation.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		lblLocation.setBounds(39, 28, 103, 50);
@@ -164,11 +162,7 @@ public class Jframe {
 		textField_1 = new JTextField();
 		textField_1.setBounds(136, 42, 200, 31);
 		frame2.add(textField_1);
-		textField_1.setColumns(10);
-		
-	/*JButton btnNewButton = new JButton("Go");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) */	
+		textField_1.setColumns(10); 
 		
 		JButton btnA = new JButton("ADD");
 		btnA.setBounds(355, 46, 69, 23);
@@ -179,8 +173,9 @@ public class Jframe {
 		frame2.add(btnNewButton_1);
 		
 		List list = new List();
-		list.setBounds(51, 100, 213, 137);
+		list.setBounds(39, 95, 240, 124);
 		frame2.add(list);
+		
 		//till here
 	}
 }
